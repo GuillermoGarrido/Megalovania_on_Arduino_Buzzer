2216bfdbb0ab925686ca738148eafdeebbbbec0a
Guillermo Garrido <ggarr018@fiu.edu>
2020-04-29 13:13:45 -0400

    Update Megalovania_on_Arduino_Buzzer.ino

--- Megalovania_on_Arduino_Buzzer.ino
+++ Megalovania_on_Arduino_Buzzer.ino
@@ -1,6 +1,32 @@
 //FUNNY MEME MAN MUSIC
 
-#define 
+int buzzer = 8;
+
+#define NOTE_C3
+#define NOTE_D3
+#define NOTE_E3
+#define NOTE_F3
+#define NOTE_G3
+#define NOTE_A3
+#define NOTE_B3
+#define NOTE_C4  262   //Defining note frequency
+#define NOTE_D4  294
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_G4  392
+#define NOTE_A4  440
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_D5  587
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_G5  784
+#define NOTE_A5  880
+#define NOTE_B5  988
+int meme_notes[] = 
+{
+  
+}
 
 void setup() {
   // put your setup code here, to run once:
